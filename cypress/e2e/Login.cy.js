c496cc7ca3ea4d32b656ddd4369b0096af6cbd80
KAVITA\kavit <kavita.mahajan@nvest.in>
2023-08-10 19:47:25 +0530

    New login program 1

--- cypress/e2e/Login.cy.js
+++ cypress/e2e/Login.cy.js
@@ -2,19 +2,24 @@
 
 describe('Login AU bank UAT', () => {
 
-  before(function(){    
-  
-      cy.visit('/')
-      cy.wait(5000)
-      cy.get('#loginbtn').click();
-      cy.wait(2000)
-  })
-
   it('tests AU bank UAT', () => {
-      
-      cy.get('#username').click();
-      cy.get('#username').type('insure');
-      cy.get('#password').click().type('123456');
+    cy.visit('http://13.234.16.249:1005/sis/inputfg.aspx')
+    
+    console.log("new test passed")
+})
+  // before(function(){    
+  
+  //     cy.visit('/')
+  //     cy.wait(5000)
+  //     cy.get('#loginbtn').click();
+  //     cy.wait(2000)
+  // })
 
-  })
+  // it('tests AU bank UAT', () => {
+      
+  //     cy.get('#username').click();
+  //     cy.get('#username').type('insure');
+  //     cy.get('#password').click().type('123456');
+
+  // })
 })
