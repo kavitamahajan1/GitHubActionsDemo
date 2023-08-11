d34ba2a8a5eb0e5888e9a96bdd828000b2e91ca3
KAVITA\kavit <kavita.mahajan@nvest.in>
2023-08-11 19:23:03 +0530

    SBI Link

--- cypress/e2e/Login.cy.js
+++ cypress/e2e/Login.cy.js
@@ -1,11 +1,14 @@
 ///<reference types="cypress"/>
 
-describe('Login AU bank UAT', () => {
+describe('Login SBI general', () => {
 
-  it('tests AU bank UAT', () => {
+  it('tests SBI General dev link', () => {
     
+    console.log('SBI general link works');
+
     cy.visit('https://dev.nvest.in:455/Login/LoginSBI')
     
-    console.log("new test pass")
+   
+    
   })
 })
