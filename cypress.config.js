44a5345883f2d8deb61efc5456ed09486f30c144
KAVITA\kavit <kavita.mahajan@nvest.in>
2023-08-29 10:24:53 +0530

    SBI Link1

--- cypress.config.js
+++ cypress.config.js
@@ -5,7 +5,7 @@ module.exports = defineConfig({
   e2e: {
     watchForFileChanges: false,
     defaultCommandTimeout: 20000,
-    //baseUrl: "https://insurance.aubankuat.in/",
+   
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
--- cypress/e2e/Login.cy.js
+++ cypress/e2e/Login.cy.js
@@ -5,7 +5,8 @@ describe('Login SBI general', () => {
   it('tests SBI General dev link', () => {
     
    // console.log('SBI general link works');
-    
-    cy.visit('https://bi.bajajallianzlife.com/SIS/Need.aspx');
+    //cy.visit('https://bi.bajajallianzlife.com/SIS/Need.aspx');
+
+    cy.visit('http://13.234.16.249:9087/Home');
   })
 })
